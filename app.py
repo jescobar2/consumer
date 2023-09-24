41264be8388b60a3365e8771406a81050b601c90
jescobar <jescobar@questrade.com>
2023-09-24 13:11:27 -0500

    adding consumer

--- app.py
+++ app.py
@@ -8,22 +8,22 @@ app = Flask(__name__)
 def hello():
     host = os.uname()[1]
 
-    received_messages = []
-    # Configuración de conexión a RabbitMQ
-    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq-service'))
-    channel = connection.channel()
-    channel.queue_declare(queue='my_cola')
+    # received_messages = []
+    # # Configuración de conexión a RabbitMQ
+    # connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq-service'))
+    # channel = connection.channel()
+    # channel.queue_declare(queue='my_cola')
 
-    def callback(ch, method, properties, body):
-        print(f"Mensaje recibido: {body}")
-        received_messages.append(body)
+    # def callback(ch, method, properties, body):
+    #     print(f"Mensaje recibido: {body}")
+    #     received_messages.append(body)
 
-    channel.basic_consume(queue='my_queue', on_message_callback=callback, auto_ack=True)
+    # channel.basic_consume(queue='my_queue', on_message_callback=callback, auto_ack=True)
 
-    print('Consumer 1 esperando mensajes. Para salir, presiona CTRL+C')
-    channel.start_consuming()
+    # print('Consumer 1 esperando mensajes. Para salir, presiona CTRL+C')
+    # channel.start_consuming()
 
-    return f"Hello, consumer fucking world!\nVersion: 1.0.0\nHostname: {host} and {os.environ['RABBITMQ_HOST']}and {received_messages} \n"
+    return f"Hello, consumer fucking world!\nVersion: 1.0.0\nHostname: {host} and {os.environ['RABBITMQ_HOST']}\n"
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', '8081')
